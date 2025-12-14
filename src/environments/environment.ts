83485223c7335890a569a6def2bf4c1055f5cfc0
mohIT-gc <you@example.com>
2025-12-13 19:44:58 -0500

    fix: give backend url in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://gmuece553-team4-rag-backend-a2fghsejh3awbncp.canadacentral-01.azurewebsites.net/'
+  backendUrl: 'https://gmuece553-team4-rag-backend-a2fghsejh3awbncp.canadacentral-01.azurewebsites.net'
 };
